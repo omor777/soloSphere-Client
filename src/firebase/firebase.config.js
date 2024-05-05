f066b24289015d19b42eb21d54b27848d2c695f8
omor777 <omor0843@gmail.com>
2024-05-06 00:37:36 +0600

    added login page and handle google login and logout

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBb7IAifR41dv4zKfK4LWjZUdZpqUsvbtQ",
+  authDomain: "market-place-daf28.firebaseapp.com",
+  projectId: "market-place-daf28",
+  storageBucket: "market-place-daf28.appspot.com",
+  messagingSenderId: "87642343605",
+  appId: "1:87642343605:web:4383e8c8192773a5ea56fc"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
--- src/layouts/shared/Navbar.jsx
+++ src/layouts/shared/Navbar.jsx
@@ -1,17 +1,28 @@
 import { Link, NavLink } from "react-router-dom";
-
-
+import useAuth from "../../hooks/useAuth";
 
 const Navbar = () => {
+  const { user, logoutUser } = useAuth();
+
+  const handleLogout = async () => {
+    try {
+      await logoutUser();
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
     <header className=" bg-base-100 shadow-md">
       <nav className="navbar container ">
         <div className="flex-1">
-          <Link to='/' className="flex items-center gap-2">
-          <img className="size-11" src="https://i.postimg.cc/mhWx1pLN/logo.png" alt="logo" />
-          <span className="text-xl font-bold text-secondary">soloSphere</span>
+          <Link to="/" className="flex items-center gap-2">
+            <img
+              className="size-11"
+              src="https://i.postimg.cc/mhWx1pLN/logo.png"
+              alt="logo"
+            />
+            <span className="text-xl font-bold text-secondary">soloSphere</span>
           </Link>
-
         </div>
         <div className="flex-none">
           <div className="dropdown dropdown-end">
@@ -22,50 +33,55 @@ const Navbar = () => {
               Home
             </NavLink>
           </div>
-          <div className="dropdown dropdown-end">
-            <div
-              tabIndex={0}
-              role="button"
-              className="btn btn-ghost btn-circle avatar"
+          {!user ? (
+            <Link
+              to="/login"
+              className="font-bold mr-5  text-lg duration-300 hover:text-secondary"
             >
-              <div className="w-10 rounded-full">
-                <img
-                  alt="Tailwind CSS Navbar component"
-                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
-                />
+              Login
+            </Link>
+          ) : (
+            <div className="dropdown dropdown-end">
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-ghost btn-circle avatar"
+              >
+                <div className="w-10 rounded-full">
+                  <img
+                    referrerPolicy="unsafe-url"
+                    alt="Tailwind CSS Navbar component"
+                    src={user && user?.photoURL}
+                  />
+                </div>
               </div>
-            </div>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow-md bg-base-100 rounded w-52 space-y-3"
-            >
-              <li>
-                <Link className="capitalize">
-                add job
-                </Link>
-              </li>
-              <li>
-              <Link className="capitalize">
-                my posted jobs
-                </Link>
-              </li>
-              <li>
-              <Link className="capitalize">
-                my bids
-                </Link>
-              </li>
-              <li>
-              <Link className="capitalize">
-                bids requests
-                </Link>
-              </li>
-              <li>
-                <button className="bg-secondary py-2 px-4 rounded text-white text-center items-center justify-center hover:bg-secondary">
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow-md bg-base-100 rounded w-52 space-y-3"
+              >
+                <li>
+                  <Link className="capitalize">add job</Link>
+                </li>
+                <li>
+                  <Link className="capitalize">my posted jobs</Link>
+                </li>
+                <li>
+                  <Link className="capitalize">my bids</Link>
+                </li>
+                <li>
+                  <Link className="capitalize">bids requests</Link>
+                </li>
+                <li>
+                  <button
+                    onClick={handleLogout}
+                    className="bg-secondary py-2 px-4 rounded text-white text-center items-center justify-center hover:bg-secondary"
+                  >
                     Logout
-                </button>
-              </li>
-            </ul>
-          </div>
+                  </button>
+                </li>
+              </ul>
+            </div>
+          )}
         </div>
       </nav>
     </header>
